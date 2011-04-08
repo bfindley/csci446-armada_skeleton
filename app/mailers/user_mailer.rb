b3264896b74900218e87964ce9195e5946abe563
Brian Findley <bfindley@mymail.mines.edu>
2011-04-08 14:05:59 -0600

    Changed the email domain name to matcth the production address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
-    from          "No Reply <no-reply@NoReply.com>"
+    from          "No Reply <no-reply@blooming-wind-193.heroku.com>"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
