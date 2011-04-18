39342c157a714cf12876f4cc849c4d37bdad9be9
Brian Findley <bfindley@mymail.mines.edu>
2011-04-17 18:24:27 -0600

    Added some more functionality to fighters

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,2 +1,4 @@
 class Fighter < ActiveRecord::Base
+belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110417203608) do
+ActiveRecord::Schema.define(:version => 20110417203609) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
