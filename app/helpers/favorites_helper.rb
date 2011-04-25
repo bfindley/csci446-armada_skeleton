2951f49da6ffe482d66c8fc7ea44d8a8e12c1a0d
Daniel Baumann <dbaumann@mines.edu>
2011-04-25 14:35:59 -0600

    Minor tweaks to favorites feature.

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,25 +1,31 @@
 module FavoritesHelper
 
   def favorite_toggle(active, path)
-    link_to(
-      image_tag('icon_small_star.png', :style => (active ? "" : "display:none"), :id => "star_on")+
-      content_tag(:span, 'Favorite added.', :style => "position: absolute; bottom: -20px; left: -20px; display:none", :id => "star_on_msg")+
-      image_tag('icon_small_star_inactive.png', :style => (active ? "display:none" : ""), :id => "star_off")+
-      content_tag(:span, 'Favorite removed.', :style => "position: absolute; bottom: -20px; left: -20px; display:none", :id => "star_off_msg"),
+    
+    toggle_on_image = image_tag('icon_small_star.png', :style => (active ? "" : "display:none"), :class => "star_on")
+    
+    toggle_on_text = content_tag(:span, 'added to favorites', :style => "position: absolute; bottom: -20px; left: -20px; display:none; width: 120px;", :class => "star_on_msg")
+    
+    toggle_off_image = image_tag('icon_small_star_inactive.png', :style => (active ? "display:none" : ""), :class => "star_off")
+    
+    toggle_off_text = content_tag(:span, 'removed from favorites', :style => "position: absolute; bottom: -20px; left: -20px; display:none; width: 150px;", :class => "star_off_msg")
+    
+    
+    link_to(toggle_on_image+toggle_on_text+toggle_off_image+toggle_off_text,
       path,
       'data-method' => :put,
       'data-remote' => true,
       :onclick => '
-        $("#star_on", this).toggle(0, function(){
+        $(".star_on", this).toggle(0, function(){
           if($(this).is(":visible"))
-            $("#star_on_msg", this.parentNode).slideToggle(500, function(){
-              $("#star_on_msg", this.parentNode).slideToggle(3000);
+            $(".star_on_msg", this.parentNode).fadeToggle(500, function(){
+              $(".star_on_msg", this.parentNode).fadeToggle(2000);
             });
         });
-        $("#star_off", this).toggle(0, function(){
+        $(".star_off", this).toggle(0, function(){
           if($(this).is(":visible"))
-            $("#star_off_msg", this.parentNode).slideToggle(500, function(){
-              $("#star_off_msg", this.parentNode).slideToggle(3000);
+            $(".star_off_msg", this.parentNode).fadeToggle(500, function(){
+              $(".star_off_msg", this.parentNode).fadeToggle(2000);
             });
         });',
       :title => "toggle as a favorite",
