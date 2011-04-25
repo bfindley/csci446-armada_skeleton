81a773c6e832dd629944a0d3c98914b9316a5736
Daniel Baumann <dbaumann@mines.edu>
2011-04-25 10:42:17 -0600

    Resolves ticket #37

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -3,12 +3,27 @@ module FavoritesHelper
   def favorite_toggle(active, path)
     link_to(
       image_tag('icon_small_star.png', :style => (active ? "" : "display:none"), :id => "star_on")+
-      image_tag('icon_small_star_inactive.png', :style => (active ? "display:none" : ""), :id => "star_off"),
+      content_tag(:span, 'Favorite added.', :style => "position: absolute; bottom: -20px; left: -20px; display:none", :id => "star_on_msg")+
+      image_tag('icon_small_star_inactive.png', :style => (active ? "display:none" : ""), :id => "star_off")+
+      content_tag(:span, 'Favorite removed.', :style => "position: absolute; bottom: -20px; left: -20px; display:none", :id => "star_off_msg"),
       path,
       'data-method' => :put,
       'data-remote' => true,
-      :onclick => '$("#star_on", this).toggle();$("#star_off", this).toggle();',
-      :title => "toggle as a favorite")
+      :onclick => '
+        $("#star_on", this).toggle(0, function(){
+          if($(this).is(":visible"))
+            $("#star_on_msg", this.parentNode).slideToggle(500, function(){
+              $("#star_on_msg", this.parentNode).slideToggle(3000);
+            });
+        });
+        $("#star_off", this).toggle(0, function(){
+          if($(this).is(":visible"))
+            $("#star_off_msg", this.parentNode).slideToggle(500, function(){
+              $("#star_off_msg", this.parentNode).slideToggle(3000);
+            });
+        });',
+      :title => "toggle as a favorite",
+      :style => "text-decoration: none; position: relative;")
   end
 
 end
